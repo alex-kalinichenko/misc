bac4ab789da69e1a782473d60912fec2dbb21c67
alex-kalinichenko <alex@kalinichenko.info>
2019-12-28 18:37:10 +0300

    Update

--- Test/Test.py
+++ Test/Test.py
@@ -1,3 +1,11 @@
-lst = []
-for _ in range(10):
-    lst.append(random.randint(-10,10))
+s = ""
+a = 3
+while a != 1:
+    s += str(a)
+
+    if a % 2 == 0:
+        a = a / 2
+    else:
+        a = 3 * a + 1
+        print(s)
+print(s)
