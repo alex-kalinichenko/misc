3a8b2af1c473fd5f71ad9f7bb64b6fa85fe1323a
alex-kalinichenko <alex@kalinichenko.info>
2020-01-01 22:58:53 +0300

    Update

--- GB_Python/Task_8.4.py
+++ GB_Python/Task_8.4.py
@@ -13,7 +13,6 @@
 # Подсказка: постарайтесь по возможности реализовать в проекте «Склад оргтехники» максимум
 # возможностей, изученных на уроках по ООП.
 
-eq_dict = {"model":}
 
 class Store:
     equipment_num = 0
@@ -32,57 +31,57 @@ class Equipment(Store):
 class Printer(Equipment):
     printer_num = 0
 
-    def __init__(self, black_color):
+    def __init__(self, brand, model, quantity, black_color):
         self.black_color = black_color
-        super().__init__(self)
+        super().__init__(brand, model, quantity)
 
+    def get_printer(self, brand, model, quantity, black_color):
+        self.black_color = black_color
+        super().__init__(brand, model, quantity)
+        printer_num += 1
+
+    def take_printer(self, brand, model, quantity, black_color):
+        self.black_color = black_color
+        super().__init__(brand, model, quantity)
+        printer_num += 1
+        print(printer_params)
 
 class Scanner(Equipment):
     scanner_num = 0
 
-    def __init__(self, network, room):
+    def __init__(self, brand, model, quantity, network):
         self.network = network
         self.room = room
-        super().__init__()
+        super().__init__(brand, model, quantity)
 
 
 class Phone(Equipment):
     phone_num = 0
 
-    def __init__(self, user):
+    def __init__(self, brand, model, quantity, user):
         self.user = user
-        super().__init__(self)
+        super().__init__(brand, model, quantity)
 
-printer_1 = Printer('HP', 'LaseJet-1020', 10)
 
-el_list = []
 
+
+equipment_dict = {}
+#                    {'LaseJet-1020': ('HP', 5, 'black')}
+"""                  модель: бренд, количество, параметр   """
 while True:
-    in_out = input('Введите 1 для приёма оргтехники на сколад и 2 для поставки в компанию: ')
-    if in_out == '1' or '2':
+    print('Вы хотите принять или поставить оргтехнику?')
+    in_out = input('1 - для приёма оргтехники на склад\n2 - для поставки в компанию\nВвод:')
+    if in_out == '1':
+        eq_type = input('\nВыбирете тип оргтехники для приёма:\n1 - принтер\n2 - сканер\n3 - телефон:')
+        if eq_type == '1':
+            printer_params = input('Введите через пробел модель, бренд, количество и дополнительные параметры:\n'
+                                   'для принтера: (black/color)\nдля сканера: сетевой (True/False)\n'
+                                   'для телефона: имя пользователя\n:').split()
+            #printer_1 = Printer(printer_params[0], printer_params[1], printer_params[2], printer_params[3])
+            Printer(printer_params[0], printer_params[1], printer_params[2], printer_params[3]).get_printer(printer_params[0], printer_params[1], printer_params[2], printer_params[3])
 
-    else
-        break
-
-
-
-
-while True:
-    try:
-        in_out = input('Введите 1 для приёма оргтехники на сколад и 2 для поставки в компанию: ')
-        if in_out == '1' or '2':
-            print(f'Работа скрипта остановлена. Сформирован список: {el_list}')
-            break
-        elif not el.isdigit():
-            raise NumCheck("Вы ввели не число")
-        # код который может вызвать исключительную ситуацию
-    except NumCheck as err:
-        print(err)
-        # что делать если возникла исключительная ситуация
-    else:
-        el = int(el)
-        el_list.append(el)
-        # что делать если ошибок не было
-    # finally:
-    # выполняется всегда
-
+            #Printer('HP', 'LaseJet-1020', 10, 'black')
+            equipment_dict.update({printer_params[0]: (printer_params[1:4])})
+            print(equipment_dict)
+    elif in_out == '2':
+        eq_type = input('\nВыбирете тип оргтехники для поставки:\n1 - принтер\n2 - сканер\n3 - телефон:')
