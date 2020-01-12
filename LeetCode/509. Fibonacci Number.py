8a6e2aa1f8b7df7511256154b6a22f224b4e307a
alex-kalinichenko <alex@kalinichenko.info>
2020-01-12 23:27:17 +0300

    Update

--- LeetCode/509. Fibonacci Number.py
+++ LeetCode/509. Fibonacci Number.py
@@ -28,3 +28,13 @@ class Solution:
 
 
 print(Solution().fib(6))
+
+# через рекурсию
+def fib_rec(n):
+    if n <= 1:
+        return 1
+    else:
+        return fib_rec(n - 1) + fib_rec(n - 2)
+
+
+print(fib_rec(6))
