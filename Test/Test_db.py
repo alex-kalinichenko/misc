a37875b87b245cb091b6d2e155ff48780bd589fb
alex <alex@kalinichenko.info>
2020-02-11 23:58:43 +0300

    Update

---
+++ Test/Test_db.py
@@ -0,0 +1,8 @@
+[MSSQLTest]
+Driver = ODBC Driver 19 for SQL Server
+Server = [protocol:]server[,port]
+#
+# Note:
+# Port is not a valid keyword in the odbc.ini file
+# for the Microsoft ODBC driver on Linux or macOS
+#
