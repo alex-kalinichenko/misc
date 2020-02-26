d823e9f1ce47d9927c0b8079cd84f1ca58464218
alex-kalinichenko <56787559+alex-kalinichenko@users.noreply.github.com>
2020-02-26 15:12:57 +0300

    Added Algo/HW and changed gitignore - added __pycache

---
+++ GB_Algorythms/HW/Lesson_8/Olga_Gurina/les_8_task_1.py
@@ -0,0 +1,13 @@
+# 1. На улице встретились N друзей.
+# Каждый пожал руку всем остальным друзьям (по одному разу).
+# Сколько рукопожатий было?
+from functools import reduce
+
+n = int(input('Сколько встретилось друзей: '))
+graph = [[int(i > j) for i in range(n)] for j in range(n)]
+
+print(*graph, sep='\n')
+count = reduce(lambda acc, x: acc + sum(x), graph, 0)
+print(f'Количество рукопожатий {count}')
+
+print(graph)
