335aa7d7feab396f6c58de838030373938b9e182
alex-kalinichenko <56787559+alex-kalinichenko@users.noreply.github.com>
2020-03-02 15:03:59 +0300

    Update

--- DB/1.db.py
+++ DB/1.db.py
@@ -9,13 +9,15 @@ with pyodbc.connect('Driver={SQL Server};'
                       'UID=user_name;'
                       'PWD=pass') as conn:
     with conn.cursor() as cursor:
+        # cursor.execute('show databases')
         cursor.execute('SELECT OIL_MASS FROM dbo.VT_WIRF_TOT_DAY_en_US')
-        #cursor.execute('SELECT START_DATETIME FROM dbo.VT_WIRF_PTEST_en_US')
-        #cursor.execute('SELECT LIQUID_MASS FROM dbo.VT_ACT_DAY_en_US')
-        #cursor.execute('SELECT CASING_PRESS1 FROM dbo.VT_WELL_READ_DAY_en_US') #  долгий запрос
-        #cursor.execute('SELECT WH_PRESS FROM dbo.VT_WELL_READ_DAY_en_US') #  долгий запрос
-        #cursor.execute('SELECT WH_PRESS FROM dbo.VT_WELL_READ_DAY_en_US') #  долгий запрос
-        #cursor.execute('SELECT CHOKE_1 FROM dbo.VT_WELL_READ_DAY_en_US') #  долгий запрос
+
+        # cursor.execute('SELECT START_DATETIME FROM dbo.VT_WIRF_PTEST_en_US')
+        # cursor.execute('SELECT LIQUID_MASS FROM dbo.VT_ACT_DAY_en_US')
+        # cursor.execute('SELECT CASING_PRESS1 FROM dbo.VT_WELL_READ_DAY_en_US') #  долгий запрос
+        # cursor.execute('SELECT WH_PRESS FROM dbo.VT_WELL_READ_DAY_en_US') #  долгий запрос
+        # cursor.execute('SELECT WH_PRESS FROM dbo.VT_WELL_READ_DAY_en_US') #  долгий запрос
+        # cursor.execute('SELECT CHOKE_1 FROM dbo.VT_WELL_READ_DAY_en_US') #  долгий запрос
     # print (df)
         for row in cursor:
             for value in row:
