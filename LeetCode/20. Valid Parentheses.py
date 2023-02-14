a9c2d0d2493de6cb5bdd59b9351393362102eb94
Alex Kalinichenko <alex@kalinichenko.info>
2023-02-14 18:04:14 +0000

    add leetcode task "Valid Parentheses"

--- LeetCode/20. Valid Parentheses.py
+++ LeetCode/20. Valid Parentheses.py
@@ -4,17 +4,45 @@
 # Open brackets must be closed by the same type of brackets.
 # Open brackets must be closed in the correct order.
 
+# Constraints:
+# 1 <= s.length <= 10^4
+# s consists of parentheses only '()[]{}'.
 
 class Solution:
     def isValid(self, s: str) -> bool:
+        brackets_dict = {'(': ')',
+                         '[': ']',
+                         '{': '}',
+                         }
+        brackets_dict_ = {')': '(',
+                          ']': '[',
+                          '}': '{',
+                         }
+        if len(s) % 2 == 1:
+            return False
+        else:
+            lifo = []
+            for char in s:
+                if char in brackets_dict.keys():
+                    lifo.append(char)
+                else:
+                    if lifo and brackets_dict_[char] == lifo[-1]:
+                        lifo.pop(-1)
+                    else:
+                        return False
+            
+            return False if lifo else True
 
-        return True
 
+assert Solution().isValid("()") == True, 'should be True'
+assert Solution().isValid("()[]{}") == True, 'should be True'
+assert Solution().isValid("{[]}") == True, 'should be True'
 
-print(Solution().isValid("()"))
-print(Solution().isValid("()[]{}"))
-print(Solution().isValid("(]"))
-print(Solution().isValid("([)]"))
-print(Solution().isValid("{[]}"))
 
 assert Solution().isValid("()") == True, 'True'
+assert Solution().isValid("())") == False, 'False'
+assert Solution().isValid("((") == False, 'False'
+assert Solution().isValid("(]") == False, 'should be False'
+assert Solution().isValid("([)]") == False, 'should be False'
+assert Solution().isValid("([)]]") == False, 'should be False'
+assert Solution().isValid("){") == False, 'should be False'
